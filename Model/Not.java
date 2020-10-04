c5ba85c64082c0b1b316b07be816c12f58347c9e
Louis Ian <louisiangt@gmail.com>
2020-10-04 11:26:41 -0300

    Negation operation

--- Model/Not.java
+++ Model/Not.java
@@ -1,6 +1,10 @@
 public class Not implements IOperation {
     private IOperation p1;
 
+    public Not(IOperation p){
+        this.p1 = p;
+    }
+
     public Boolean value(){
         return !this.p1.value();
     }
