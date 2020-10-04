04dc41ffb91074ca3bc82d28c79db3ecec63c016
Louis Ian <louisiangt@gmail.com>
2020-10-04 11:26:54 -0300

    composite test

--- Model/Main.java
+++ Model/Main.java
@@ -4,8 +4,13 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner keyboard = new Scanner(System.in);
+
+		Proposition a = new Proposition(true);
+		Proposition b = new Proposition(true);
 		
-		IOperation op;
+		IOperation op = new And(a, new Not(b));	// A and ¬B
+
+		System.out.println(op.value());
 		
  		keyboard.close();
          
